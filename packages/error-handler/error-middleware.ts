0b75a94e40bffbfcbc93f4ac6e02909c3c8ca4f6
Siddharth <siddharthsingh306401@gmail.com>
2026-04-14 14:19:27 +0530

    api-doc bna rha tha

--- packages/error-handler/error-middleware.ts
+++ packages/error-handler/error-middleware.ts
@@ -1,6 +1,6 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { AppError } from "./index";
-export const errorMiddleware = (err: Error, req: Request, res: Response) => {
+export const errorMiddleware = (err: Error, req: Request, res: Response, next: NextFunction) => {
   if (err instanceof AppError) {
     console.log(`Error: ${req.method} ${req.url} - ${err.message}`);
 
--- packages/libs/reddis/index.ts
+++ packages/libs/reddis/index.ts
@@ -1,20 +1,5 @@
 import Redis from "ioredis";
 
-const rawRedisHost = process.env.REDIS_HOST || "127.0.0.1";
-const normalizedRedisHost = rawRedisHost
-  .replace(/^redis:\/\//, "")
-  .replace(/^rediss:\/\//, "")
-  .replace(/^https?:\/\//, "")
-  .split("/")[0];
-const shouldUseTls =
-  /^(rediss|https):\/\//.test(rawRedisHost) ||
-  normalizedRedisHost.includes("upstash.io");
-
-const redis = new Redis({
-  host: normalizedRedisHost,
-  port: Number(process.env.REDIS_PORT) || 6379,
-  password: process.env.REDIS_PASSWORD,
-  ...(shouldUseTls ? { tls: {} } : {}),
-});
+const redis = new Redis(process.env.REDIS_DATABSE_URL!);
 
 export default redis;
