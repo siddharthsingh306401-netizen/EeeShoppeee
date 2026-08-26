9156c17ffee2578f5fdaff0645363c090980e7a0
Neha <kumarineha.su0@gmail.com>
2026-08-26 13:44:47 +0530

    Add payment component

---
+++ dashboard ui/account-icon.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const AccountsIcon = ({ fill }: { fill: string }) => {
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className="nextui-c-PJLV nextui-c-PJLV-ibxboXQ-css"
+    >
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M3 5V19C3 20.1 3.89 21 5 21H19C20.1 21 21 20.1 21 19V5"
+        fill={fill}
+      ></path>
+    </svg>
+  );
+};
+
+export default AccountsIcon;
